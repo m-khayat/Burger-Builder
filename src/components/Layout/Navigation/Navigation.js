a08d4a2f11b36b48fb1679116242657eafa99b8e
mohamad_khayat <mohameedkhayat98@gmail.com>
2023-10-24 06:54:09 +0300

    v2

--- src/components/Layout/Navigation/Navigation.js
+++ src/components/Layout/Navigation/Navigation.js
@@ -2,8 +2,8 @@ import React from "react";
 import "./Navigation.css"
 const navigation = (props) => (
     <ul className="Nav">
-        <li><a href="/" >Burger Builder</a></li>
-        <li><a href="/" >Checkout</a></li>
+        <li><a href="#" >Burger Builder</a></li>
+        <li><a href="#" >Checkout</a></li>
     </ul>
 );
 
